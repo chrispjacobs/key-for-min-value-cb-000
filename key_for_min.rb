7e9cd4bc9d2509ab280ca4e993758b8bac8ca9cd
Christopher Jacobs <chrisj.3222@gmail.com>
2019-03-05 20:29:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,8 +10,6 @@ def key_for_min_value(name_hash)
     elsif value < winning_value
       winning_value = value
       winning_key = key
-    elsif value == winning_value
-      nil
     end
   end
 winning_key
