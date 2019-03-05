8224d37204127f83e14eac06a7b6fb33e67fd81d
Christopher Jacobs <chrisj.3222@gmail.com>
2019-03-05 20:34:04 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,12 +4,12 @@
 def key_for_min_value(name_hash)
   winning_value = Float::INFINITY
   winning_key = "loser"
-  name_hash.each do |key, value|
-    if name_hash.class != Hash
-      winning_key = nil
-    elsif name_hash == Hash && name_hash.empty?
-      winning_key = nil
-    elsif value <= winning_value
+  if name_hash.class != Hash
+    winning_key = nil
+  elsif name_hash == Hash && name_hash.empty?
+    winning_key = nil
+  else name_hash.each do |key, value|
+    if value <= winning_value
       winning_value = value
       winning_key = key
     end
