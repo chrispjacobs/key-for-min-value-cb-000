b90ce2b37970ecb66b8736797e391f4fe969dbb4
Christopher Jacobs <chrisj.3222@gmail.com>
2019-03-05 20:35:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,10 +8,12 @@ def key_for_min_value(name_hash)
     winning_key = nil
   elsif name_hash == Hash && name_hash.empty?
     winning_key = nil
-  else name_hash.each do |key, value|
-    if value <= winning_value
-      winning_value = value
-      winning_key = key
+  else
+    name_hash.each do |key, value|
+      if value <= winning_value
+        winning_value = value
+        winning_key = key
+      end
     end
   end
 winning_key
