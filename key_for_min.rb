6a401a33b6f4ef6403f40f761a210260effbde91
Christopher Jacobs <chrisj.3222@gmail.com>
2019-03-05 20:31:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,9 +6,9 @@ def key_for_min_value(name_hash)
   winning_key = "loser"
   name_hash.each do |key, value|
     if name_hash.class != Hash
-      nil
+      winning_key = nil
     elsif name_hash == Hash && name_hash.empty?
-      nil
+      winning_key = nil
     elsif value <= winning_value
       winning_value = value
       winning_key = key
